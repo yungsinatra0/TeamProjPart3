c9cc48f4c44d483ca4961e121a1e5441ef3e184d
ccorcimaru <calincor@gmail.com>
2023-03-13 23:32:06 +0000

    added initial links to subsystems (data/chat)

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-    <b> This is the dashboard lol</b>
+    <b> This is the dashboard lol</b> <br>
+
+    <NuxtLink to="/chat">Go to Chat subsystem</NuxtLink> <br>
+    <NuxtLink to="/data">Go to Data Analytics subsystem</NuxtLink>
 </template>
 
 <style>
