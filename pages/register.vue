2cbd055df640e678b884607617c50613d09a1b4f
ccorcimaru <calincor@gmail.com>
2023-03-13 22:21:50 +0000

    initial working register page created

---
+++ pages/register.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+const form = ref<HTMLFormElement>()
+
+async function onSubmit() {
+    const data = {
+        name: form.value!.user.value,
+        password: form.value!.password.value
+    }
+
+    const result = await $fetch("/api/register", {
+        method: "POST",
+        body: data,
+    })
+
+	navigateTo("/dashboard")
+}
+
+</script>
+
+<template>
+	<form @submit.prevent="onSubmit" class="form" ref="form">
+		<label id="title">Member Registration</label>
+		<label for="user">User:</label>
+		<div class="">
+			<input
+				type="text"
+				id="User"
+				placeholder="Example"
+				required
+                name="user"
+			/>
+		</div>
+		<label for="Password">Password:</label>
+		<input
+			type="password"
+			id="Password"
+			name="password"
+			required
+		/>
+		<Button type="submit" icon="material-symbols:login-rounded">Register</Button>
+	</form>
+</template>
+
+<style>
+</style>
