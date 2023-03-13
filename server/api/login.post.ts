2601c2517e4a791f492fd9c2f9d2f856b92f529f
ccorcimaru <calincor@gmail.com>
2023-03-13 22:08:21 +0000

    added password verification for user login

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -4,12 +4,11 @@ export default defineEventHandler(async event => {
 	const body = await readBody(event)
 	
 	const user = await prisma.user.findUnique({
-		where: { email: body.email as string },  // Find user by their email address.
+		where: { name: body.name as string },  // Find user by their email address.
 	})
 
-	// return await prisma.user.findUnique({
-	// 	where: { uid: user!.uid }
-	// })
+	if (user === null) return null
+	if (user!.password !== body.password as string) return null
 
-    return user || null // Returns the user's data as an object.
+    return user // Returns the user's data as an object.
 })
