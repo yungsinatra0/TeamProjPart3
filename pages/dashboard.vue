ec202c1c015ee6233413ad99f3eeeb7021e08784
roshnis2001 <119610025+roshnis2001@users.noreply.github.com>
2023-03-17 11:56:26 +0000

    testing dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -7,6 +7,31 @@
 
     <NuxtLink to="/chat">Go to Chat subsystem</NuxtLink> <br>
     <NuxtLink to="/data">Go to Data Analytics subsystem</NuxtLink>
+    <div id="myNav" class="navbar">
+	<button class="button button1">TEXT CHAT</button>
+	<button class="button button2">DATA ANALYTICS</button>
+ 
+ 
+</div>
+
+<div>
+	<button class="button button3" style="width: 24px; text-align:center; vertical-align:middle">LOGOUT</button>
+
+</div>
+
+
+<body>
+
+<div>
+<h1 style="color:black">Employee name's Dashboard </h1>
+</div>
+
+<div class="box1">Text</div>
+
+<div class="box2">insert text</div>
+
+<div class="box3">insert text</div>
+</body>
 </template>
 
 <style>
