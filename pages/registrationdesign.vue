1bef713b9039146a887bb9a2fd3ef979138ba93f
ccorcimaru <calincor@gmail.com>
2023-03-21 20:36:02 +0000

    initial register merge & cleaned regdesign file

--- pages/registrationdesign.vue
+++ pages/registrationdesign.vue
@@ -1,12 +1,4 @@
 <template>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Registration</title>
-  </head>
-  <body>
     <div class="container">
       <div class="form">
         <div class="contact-info">
@@ -32,8 +24,6 @@
         </div>
       </div>
     </div>
-  </body>
-</html>
 </template>
 
 <style>
