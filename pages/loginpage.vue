2160ef86a8f2ad6851abc1011198712d9e38d810
ccorcimaru <calincor@gmail.com>
2023-03-21 13:09:06 +0000

    removed links to css files

--- pages/loginpage.vue
+++ pages/loginpage.vue
@@ -6,7 +6,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-    <link rel="stylesheet" href="loginstyle.css" />
   </head>
   <body>
     <div class="container">
--- pages/registrationdesign.vue
+++ pages/registrationdesign.vue
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Registration</title>
-    <link rel="stylesheet" href="registrationstyle.css" />
   </head>
   <body>
     <div class="container">
