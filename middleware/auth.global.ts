174fbd6a567957ab3d8a57100048787d905ac1ea
ccorcimaru <calincor@gmail.com>
2023-03-21 16:40:08 +0000

    added redirect to login page called when user is not logged in and accessing private pages

---
+++ middleware/auth.global.ts
@@ -0,0 +1,12 @@
+const publicRoutes = ["/login", "/register"] // Pages accessible to the public
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+	const { data: user } = await currentUser()
+	const isAuth = user.value !== null // Check if user is authenticated
+
+	const isPublic = (route: typeof to) =>
+		publicRoutes.includes(route.path ?? "") // Check if page user tries to access is public
+
+	if (!isPublic(to) && !isAuth)
+		return navigateTo("/login") // If user is not authenticated and tries to access a private page, redirect to login page
+})
