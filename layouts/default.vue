69b22b057681bc86fdd9c9eb29eddffaace50d75
ccorcimaru <calincor@gmail.com>
2023-03-21 16:38:19 +0000

    cleaned default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-import { getCurrentUser } from '~~/composables/currentUser';
-
-const { data: user } = await getCurrentUser()
-
-// Get all users in the database
-async function getUsers() {
-    const res = await $fetch('/api/users')
-    console.log(res)
-}
-
 </script>
 
 <template>
     <div>
         <h1>I'm the default layout that is used on every page (unless explicitly specified)!</h1>
-       <!-- <h2> Welcome, {{ user!.name }} </h2> -->
-
-        <button @click="getUsers">Get Users</button>
         <slot></slot>
     </div>
 </template>
