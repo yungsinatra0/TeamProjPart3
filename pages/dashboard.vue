0931bd0d4c3da0021820f4a10b00e790670edf8d
ccorcimaru <calincor@gmail.com>
2023-03-21 20:53:06 +0000

    removed unnecessary html tags & navbar/logout

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,50 +1,16 @@
 <script setup lang="ts">
-
-
 </script>
 
 <template>
+	<div>
+		<h1 style="color:black">Employee name's Dashboard </h1>
+	</div>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="robots" content="noindex, nofollow" />
-<title>Dashboard</title>
+	<div class="box1">Text</div>
 
-</head>
-    <b> This is the dashboard lol</b> <br>
+	<div class="box2">insert text</div>
 
-    <NuxtLink to="/chat">Go to Chat subsystem</NuxtLink> <br>
-    <NuxtLink to="/data">Go to Data Analytics subsystem</NuxtLink>
-
-
-<div id="myNav" class="navbar">
-	<button class="button button1">TEXT CHAT</button>
-	<button class="button button2">DATA ANALYTICS</button>
- 
- 
-</div>
-
-<div>
-	<button class="button button3" style="width: 24px; text-align:center; vertical-align:middle">LOGOUT</button>
-
-</div>
-
-
-<body>
-
-<div>
-<h1 style="color:black">Employee name's Dashboard </h1>
-</div>
-
-<div class="box1">Text</div>
-
-<div class="box2">insert text</div>
-
-<div class="box3">insert text</div>
-</body>
-</html>
+	<div class="box3">insert text</div>
 </template>
 
 <style type="text/css">
