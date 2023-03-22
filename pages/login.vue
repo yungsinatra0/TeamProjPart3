393b311058fbb6ad5217b87d3aa5c0222e656009
ccorcimaru <calincor@gmail.com>
2023-03-22 10:13:20 +0000

    removed head tags

--- pages/login.vue
+++ pages/login.vue
@@ -28,12 +28,6 @@ async function onSubmit(){
 </script>
 
 <template>
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
-  </head>
   <body>
     <div class="container">
       <div class="form">
--- pages/register.vue
+++ pages/register.vue
@@ -19,11 +19,6 @@ async function onSubmit() {
 </script>
 
 <template>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Register</title>
-  </head>
   <body>
     <div class="container">
       <div class="form">
