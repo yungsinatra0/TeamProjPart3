7eb72af8e38529758becd6db1746d607e8fe8e82
ccorcimaru <calincor@gmail.com>
2023-03-23 10:59:18 +0000

    parameters should be type number, not string

--- server/api/project/[uid].get.ts
+++ server/api/project/[uid].get.ts
@@ -2,7 +2,7 @@ import prisma from "@/prisma"
 
 export default defineEventHandler( event => {
     return prisma.project.findUnique({
-        where: { uid: event.context.params.uid as string }
+        where: { uid: +(event.context.params.uid as string) }
     })
 })
 
--- server/api/task/[uid].get.ts
+++ server/api/task/[uid].get.ts
@@ -2,6 +2,6 @@ import prisma from "@/prisma"
 
 export default defineEventHandler( event => {
     return prisma.task.findUnique({
-        where: { uid: event.context.params.uid as string }
+        where: { uid: +(event.context.params.uid as string) }
     })
 })
