707a32e37f5b02781ad3d87d6e1c053715e256df
Niha <100951065+Niha-03@users.noreply.github.com>
2023-04-20 11:08:12 +0100

    Create App.vue

---
+++ pages/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+  
+   
+    <button @click="individualPerformance">{{ buttontext }}</button>
+  
+    <bar-chart v-if="displayChart" :chart-data="chartData" :options="chartOptions"></bar-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BarChart,
+  },
+  data() {
+    return {
+      displayChart: false,
+      buttontext:"Click here to view individual performance",
+      chartData: {
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        datasets: [
+          {
+            label: 'Number of tasks complete in a week',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      chartOptions: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    individualPerformance () {
+      this.displayChart = !this.displayChart;
+      if(this.displayChart)
+      {
+        this.buttontext = "Close"
+      }
+      else
+      {
+        this.buttontext = "Click here to view individual performance"
+      }
+    
+    }
+  }
+};
+
+
+</script>
