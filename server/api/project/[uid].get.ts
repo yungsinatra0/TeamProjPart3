47503a036b79e443a06ff45152e586b95d0ccbb4
SBinning01 <91889832+SBinning01@users.noreply.github.com>
2023-04-28 10:50:28 +0100

    added include

--- server/api/project/[uid].get.ts
+++ server/api/project/[uid].get.ts
@@ -2,7 +2,11 @@ import prisma from "@/prisma"
 
 export default defineEventHandler( event => {
     return prisma.project.findUnique({
-        where: { uid: +(event.context.params.uid as string) }
+       where: { uid: +(event.context.params.uid as string) },
+        include: {
+            tasks: true,
+            users: true
+        }
     })
 })
 
