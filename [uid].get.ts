2fce80e99be520a36d88b67047692e983509dce2
SBinning01 <91889832+SBinning01@users.noreply.github.com>
2023-04-28 10:45:05 +0100

    Hardcoded dropdown with two projects for testing

---
+++ [uid].get.ts
@@ -0,0 +1,13 @@
+import prisma from "@/prisma"
+
+export default defineEventHandler( event => {
+    return prisma.project.findUnique({
+       where: { uid: +(event.context.params.uid as string) },
+        include: {
+            tasks: true,
+            users: true
+        }
+    })
+})
+
+
