47bde775215bfbbc6b495a5ab69f00e1274f29be
SBinning01 <91889832+SBinning01@users.noreply.github.com>
2023-04-28 10:49:56 +0100

    hardcoded dropdown for testing

--- pages/data.vue
+++ pages/data.vue
@@ -3,10 +3,26 @@
 		<button @click="toggleChart">{{ buttontext }}</button>
 		<DataBar :label="'Number of tasks per project'" :data="projectTasks" v-if="displayIndividual"></DataBar>
 		<DataBar :label="'Number of users per project'" :data="projectUsers" v-else></DataBar>
+
+		<div id="dropdown">
+			<select @change="selectProject($event)">
+				<option value="project1">Project 1</option>
+				<option value="project2">Project 2</option>
+			</select>
+		</div>
+
+		
+		<DataBar :label="'Project 1'" :data="projectTasks" v-if="displayProj1"></DataBar>
+		<DataBar :label="'Project 2'" :data="projectUsers" v-if="displayProj2"></DataBar>
+
+
+		
 	</div>
 </template>
 
 <script setup lang="ts">
+import tasks from '~/server/api/tasks'
+
 
 const { data: currentUser } = await getCurrentUser() // get the current user from the server using the cookie
 const { data: projects } = await useFetch("/api/projects") // get all the projects from the server
@@ -14,12 +30,36 @@ const { data: projects } = await useFetch("/api/projects") // get all the projec
 // const { data: users } = await useFetch("/api/users") // get all the users from the server
 
 const displayIndividual = ref(true)
-const buttontext = ref("Click here to view individual performance")
+const displayProj1 = ref(true)
+const displayProj2 = ref(false)
+
+const buttontext = ref("Number of users per project")
 
 function toggleChart() {
 	displayIndividual.value = !displayIndividual.value
-	buttontext.value = displayIndividual.value ? "Click here to view individual performance" : "Click here to view team performance"
+	buttontext.value = displayIndividual.value ? "Number of users per project" : "Number of tasks per project"
 }
+
+function selectProject(event: any){
+	//alert(event.target.value)
+	if (event.target.value == "project1"){
+		displayProj2.value = false
+		displayProj1.value = true
+	}
+	else if (event.target.value == "project2"){
+		displayProj1.value = false
+		displayProj2.value = true	
+	}
+	
+}
+
+//get number of tasks each team member of project is assigned to
+const userTasks = computed(() => {
+	const usersAndTasks: { [key: string]: number } = {}
+
+	
+})
+
 
 // get number of tasks per project
 const projectTasks = computed(() => {
@@ -42,5 +82,5 @@ const projectUsers = computed(() => {
 	return projectAndUsers
 })
 
-console.log(projectTasks.value)
+//console.log(projectTasks.value)
 </script>
