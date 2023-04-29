27e2d2fc5ee8c688a1796bd4e7e1e27e74d194fa
ccorcimaru <calincor@gmail.com>
2023-04-29 14:28:47 +0100

    created api for fetching chats

---
+++ server/api/chats.ts
@@ -0,0 +1,21 @@
+import prisma from "@/prisma"
+import { User } from "@prisma/client"
+
+export default defineEventHandler(async event => {
+    const query = getQuery(event) // If there is a query string, it will be parsed and returned as an object.
+    
+    // Returns all projects in the database.
+    return await prisma.chat.findMany({
+        where: {
+            // Find chats where the user is a member.
+            users: {
+                some: {
+                    uid: query.uid as User["uid"]
+                }
+            }
+        },
+        include: {
+            users: true
+        }
+    })
+})
