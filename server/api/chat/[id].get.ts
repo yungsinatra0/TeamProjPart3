4d827543859a15db7d4c38fa5b8e96f0e8297ce6
ccorcimaru <calincor@gmail.com>
2023-04-29 14:34:09 +0100

    created api to fetch specific chat info

---
+++ server/api/chat/[id].get.ts
@@ -0,0 +1,11 @@
+import prisma from "@/prisma"
+
+export default defineEventHandler( event => {
+    return prisma.chat.findUnique({
+        where: { uid: +(event.context.params.uid as string) },
+        include: {
+            users: true,
+            messages: true
+        }
+    })
+})
