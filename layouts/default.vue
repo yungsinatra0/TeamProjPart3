53109e23ef7eb694029ea9163de6e71a5c36a115
ccorcimaru <calincor@gmail.com>
2023-05-13 22:20:34 +0100

    changed navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,48 @@
 <script setup lang="ts"></script>
 
 <template>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-<nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-		<NuxtLink :to="`/chat`"> <button class="button button1">Text Chat</button> </NuxtLink>
-      </li>
-      <li class="nav-item">
-        <NuxtLink :to="`/data`"> <button class="button button1">Data Analytics</button> </NuxtLink>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-			<button
-				class="button button3"
-				style="width: 24px; text-align: center; vertical-align: middle"
-				@click="deleteCookie"
-			>
-				LOGOUT
-			</button>
+	<nav id="nav">
+		<div class="nav-left">
+			<NuxtLink :to="`/chat`">
+				<button class="button button1">Text Chat</button>
+			</NuxtLink>
+			<NuxtLink :to="`/data`">
+				<button class="button button1">Data Analytics</button>
+			</NuxtLink>
 		</div>
-	</div>
-
+		<div class="nav-right">
+			<button class="nav-button" @click="deleteCookie">LOGOUT</button>
+		</div>
+	</nav>
 	<slot></slot>
 </template>
-
 <style>
-#nav{
-	background-color: azure;
+#nav {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #f8f9fa;
+}
+
+.nav-left {
+	display: flex;
+}
+
+.nav-right {
+	display: flex;
+}
+
+.nav-button {
+	background-color: #6c757d;
+	border: none;
+	color: white;
+	padding: 0.5rem;
+	margin: 0 0.5rem;
+	cursor: pointer;
+	border-radius: 5px;
+}
+
+.nav-button:hover {
+	background-color: #495057;
 }
 </style>
