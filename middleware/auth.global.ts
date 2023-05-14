f42feb723a662eb6735013fbc33b8a8322c84263
ccorcimaru <calincor@gmail.com>
2023-05-14 15:09:49 +0100

    fixed issue with not being logged in properly

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,7 +2,7 @@ const publicRoutes = ["/login", "/register"] // Pages accessible to the public
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
 	const { data: user } = await getCurrentUser()
-	const isAuth = user.value !== null // Check if user is authenticated
+	const isAuth = user.value!.body !== null // Check if user is authenticated
 
 	const isPublic = (route: typeof to) =>
 		publicRoutes.includes(route.path ?? "") // Check if page user tries to access is public
